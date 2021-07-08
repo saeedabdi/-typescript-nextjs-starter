f32fc443abcadd58e0327d1da6b62c0ef1e792ab
Saeed Abdi <saeedabdi25.s@gmail.com>
2021-07-09 01:41:36 +0430

    initialize project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div className="w-full flex justify-center items-center h-screen bg-purple-400 text-white font-bold">
+            hello docker ssusss
+        </div>
+    );
+};
+
+export default Index;
